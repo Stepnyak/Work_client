4ff7d24008aace972144c7410d51c752cd6bf5ce
Vladimir Kursinov <kursinov@mail.ru>
2022-04-17 19:10:52 +0300

    finish commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const numerOfFilms = +window.prompt("Сколько фильмов вы посмотрели, '' ");
 const lastFilm = window.prompt("Один из последних просмотренных фильмов?");
 const reyting = +window.prompt("На сколько оцените его?");
+const lastFilm1 = window.prompt("Один из последних просмотренных фильмов?");
+const reyting1 = +window.prompt("На сколько оцените его?");
 
 //console.log(`Посмотрел  ${numerOfFilms} фильмов`);
 
@@ -11,5 +13,8 @@ const personalMoviesDB = {
   genres: [],
   privat: false,
 };
-const result = `${lastFilm}:${reyting}`;
-console.log(personalMoviesDB.movies = result);
+personalMoviesDB.movies[lastFilm] = reyting;
+personalMoviesDB.movies[lastFilm1] = reyting1;
+//const result = `${lastFilm}:${reyting}`;
+//console.log(personalMoviesDB.movies = result);
+console.log(personalMoviesDB.movies);
