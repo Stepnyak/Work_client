a9f0c87eea5007935e519eb31e9da10e497156c9
Vladimir Kursinov <kursinov@mail.ru>
2022-04-17 08:24:33 +0300

    Первый commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const as = () => {
+ return console.log("this is sampl");
+};
+
+// document.querySelector('p').textContent = 'asgjggjg';
+
+console.log(`Это функция ${as}`);
+// console.log(typeof 123n)
