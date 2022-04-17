ed9238226b082812603b04b72672ba801bb5f87d
Vladimir Kursinov <kursinov@mail.ru>
2022-04-17 11:19:31 +0300

    Second questsion -Yes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-const numerOfFilms =  window.prompt("Сколько фильмов вы посмотрели ");
+const numerOfFilms = window.prompt("Сколько фильмов вы посмотрели ");
 
-console.log(`Посмотрел  ${numerOfFilms} фильмов`);
+//console.log(`Посмотрел  ${numerOfFilms} фильмов`);
+
+const personalMoviesDB = {
+  count: numerOfFilms,
+  movies: "",
+  actors: "",
+  genres: [],
+  privat: false,
+};
+console.log(personalMoviesDB.count);
