0f215124da24b4f086eb45695acc95642e233ac5
Vladimir Kursinov <kursinov@mail.ru>
2022-04-17 12:18:34 +0300

    Задание окончено

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const numerOfFilms = window.prompt("Сколько фильмов вы посмотрели ");
+const numerOfFilms = +window.prompt("Сколько фильмов вы посмотрели ");
+const lastFilm = window.prompt("Один из последних просмотренных фильмов?");
+const reyting = +window.prompt("На сколько оцените его?");
 
 //console.log(`Посмотрел  ${numerOfFilms} фильмов`);
 
@@ -9,4 +11,5 @@ const personalMoviesDB = {
   genres: [],
   privat: false,
 };
-console.log(personalMoviesDB.count);
+const result = `${lastFilm}:${reyting}`;
+console.log(personalMoviesDB.movies = result);
