2b5891796dd7b8e9dc3e315873ab42d231b728a7
Vladimir Kursinov <kursinov@mail.ru>
2022-04-18 18:29:31 +0300

    Задание 2 выполнил

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 const numerOfFilms = +window.prompt("Сколько фильмов вы посмотрели, '' ");
-const lastFilm = window.prompt("Один из последних просмотренных фильмов?");
+let lastFilm = window.prompt("Один из последних просмотренных фильмов?", "");
+while (lastFilm.length > 50 && lastFilm != "") {
+  lastFilm = window.prompt("Один из последних просмотренных фильмов?", "");
+}
 const reyting = +window.prompt("На сколько оцените его?");
-const lastFilm1 = window.prompt("Один из последних просмотренных фильмов?");
-const reyting1 = +window.prompt("На сколько оцените его?");
-
-//console.log(`Посмотрел  ${numerOfFilms} фильмов`);
 
 const personalMoviesDB = {
   count: numerOfFilms,
@@ -13,8 +12,32 @@ const personalMoviesDB = {
   genres: [],
   privat: false,
 };
-personalMoviesDB.movies[lastFilm] = reyting;
-personalMoviesDB.movies[lastFilm1] = reyting1;
-//const result = `${lastFilm}:${reyting}`;
-//console.log(personalMoviesDB.movies = result);
-console.log(personalMoviesDB.movies);
+// personalMoviesDB.movies[lastFilm] = reyting;
+//console.log(personalMoviesDB.movies);
+
+const col = personalMoviesDB.count;
+console.log(col);
+if (col < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (col >=10 && col < 30) {
+  console.log("Вы классический зритель");
+} else if (col > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
+// switch (personalMoviesDB.count) {
+//   case "< 10":
+//     console.log("Просмотрено довольно мало фильмов");
+//     break;
+//   case "= 10 && < 30":
+//     console.log("Просмотрено довольно мало фильмов");
+//     break;
+//   case "> 30":
+//     console.log("Вы киноман");
+//     break;
+//   default:
+//     console.log("Произошла ошибка");
+//     break;
+// }
